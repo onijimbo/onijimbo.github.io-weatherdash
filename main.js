47704052b82216c23d5d9dc397f7d778765e2edc
Riley Hall <onijimbo@gmail.com>
2020-01-22 16:43:15 -0800

    01-22

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-let city = $("#citySearch")
+let city =  document.getElementById("citySearch").value
+
 
 function renderBttn(){
   let cityDiv = $("<div class='name'>");
@@ -8,9 +9,25 @@ function renderBttn(){
   cityDiv.append(newCity); 
   $("#cityView").prepend(cityDiv); 
 }
-
-let queryUrl = "https://api.openweathermap.org/data/2.5/forecast?q=Seattle" + "407dcbd909ab3fe803d6bfc0fc8541ab"
+// http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=
+let queryUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&APPID=407dcbd909ab3fe803d6bfc0fc8541ab"
 
 $("#searchBttn").click(function(){
-    renderBttn()
-});
+  $.ajax({
+    url: queryUrl,
+    method: 'get' 
+  })
+    .then(function(response){
+      console.log(response)
+    });
+renderBttn()
+  });
+      
+  
+
+let cities = ['Seattle', 'Las vegas'];
+localStorage.setItem('cities', JSON.stringify(cities))
+let searchHistory = JSON.parse(localStorage.getItem('cities'))
+console.log(searchHistory)
+// number below is the dt from api response
+// new Date(1579737600).getDate() 
