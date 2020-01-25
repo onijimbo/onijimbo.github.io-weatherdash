82db60d08726a07f4a60be1dc692981f43ebf9d9
Riley Hall <onijimbo@gmail.com>
2020-01-24 18:05:32 -0800

    added ajax for uv index

--- main.js
+++ main.js
@@ -1,27 +1,56 @@
-let city =  document.getElementById("citySearch").value
 
 
-function renderBttn(){
+function renderBttn(searchEl){
   let cityDiv = $("<div class='name'>");
   let newCity = $("<button>");
-  newCity.text(document.getElementById("citySearch").value);
+  // newCity.text(document.getElementById("citySearch").value);
+  newCity.text(searchEl)
   newCity.addClass("cityBttn");
   cityDiv.append(newCity); 
   $("#cityView").prepend(cityDiv); 
+};
+
+function getWeather(searchEl) {
+  $.ajax({
+      url: "https://api.openweathermap.org/data/2.5/forecast?q="+ searchEl +"&APPID=407dcbd909ab3fe803d6bfc0fc8541ab",
+      method: 'get' 
+    })
+      .then(function(response){
+        console.log(response)
+        var tempF = (response.list[0].main.temp - 273.15) * 1.80 + 32;
+       
+        
+          renderBttn(searchEl)
+        // $("#cityDate").append(response.list[0].dt_txt)
+      // $("#temp").append(Math.floor(tempF))
+      // $("#hum").append(response.list[0].main.humidity)
+      // $("#speed").append(response.list[0].wind.speed) 
+      
+      }); 
 }
-// http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=
-let queryUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&APPID=407dcbd909ab3fe803d6bfc0fc8541ab"
+
+function uvIndex(response){
+        let uvLat = (response.city.coord.lat)
+        let uvLon = (response.city.coord.lon)
+        $.ajax({
+          url: "http://api.openweathermap.org/data/2.5/uvi/forecast?appid=407dcbd909ab3fe803d6bfc0fc8541ab&lat="+ uvLat +"&lon="+ uvLon +"&cnt={cnt}",
+          method: 'get'
+        })
+        .then(function(response2){
+          console.log(response2)
+          }) 
+          
+      }
+
 
 $("#searchBttn").click(function(){
-  $.ajax({
-    url: queryUrl,
-    method: 'get' 
-  })
-    .then(function(response){
-      console.log(response)
-    });
-renderBttn()
+  let searchEl = document.getElementById("citySearch").value;
+  getWeather(searchEl)  
+  uvIndex()  
+
   });
+
+  
       
   
 
